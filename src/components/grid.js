a49a6e042a90a82312a35538ebfc69e68f040d8d
Alec Di Vito <divito.alec@gmail.com>
2021-08-01 13:41:18 -0400

    Final mobile layout fixes

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,13 +1,31 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Grid = ({ children, className, cols, rows }) => {
+const Grid = ({ children, className, cols, rows, mobileCols, mobileRows }) => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    let colNumbers = cols;
+    let rowNumbers = rows;
+    if (mobileCols && width <= 900) {
+        colNumbers = mobileCols;
+    }
+    if (mobileRows && width <= 900) {
+        rowNumbers = mobileRows;
+    }
     let gridCols = '';
-    for (let i = 0; i < cols; i++) {
+    for (let i = 0; i < colNumbers; i++) {
         gridCols += '1fr '
     }
     let gridRows = ''
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < rowNumbers; i++) {
         gridRows += '1fr '
     }
     return <div className={className ?? ""} style={{
@@ -24,6 +42,7 @@ Grid.propTypes = {
     cols: PropTypes.number,
     rows: PropTypes.number,
     children: PropTypes.node,
+    listOnMobile: PropTypes.bool
 }
 
 Grid.defaultProps = {
